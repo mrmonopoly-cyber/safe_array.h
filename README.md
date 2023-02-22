6a4aaf0b62b4d081d4fd092373c0b5cb3c2078c1
mrmonopoly-cyber <alberto.damo@gmail.com>
2023-02-22 14:48:20 +0100

    adding documentation

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # safe_array.h
+
+this is the first version of the library and beacause of that is not prefect 
+
+
+The challenge of the library is to remove the unsafe aspect of the array in C while maintaining the most important aspect as : direct access, contiguos memory allocation.
+For now the array you create is filled from the start to the end. When you want to insert a new item you have to pass the value of the proprieties of the object, not the object itself.
+
+
+//PROBLEMS
+There is still a bug: where you ask for an item in the array, the system will give you a poiter to the data.  with that pointer you can do whatever you want, even exced the limit of the array and go beyong.
+The reason for that is that i don't wanna return a copy of the data to not waste memory. 
+
+
+
+//NOT IMPLEMENTED
+Probably i will introduce a filter dependign of the size of the data, if the data is in the size of a block of memory (4096 kB) i will create a copy and return a pointer to the copy. otherwise i will
+return the pointer to data itself like now.
+
+
+
+
--- safe_array.h
+++ safe_array.h
@@ -1,13 +1,37 @@
 #ifndef safe_array
 #define safe_array
 
-typedef void * array;	// the array is NOT omogeneus
+typedef void * array;
 
-array *new_array(unsigned int dimensions_number_size,unsigned int size_single_element,void (*allocation_value_function) (void *,void *)); // create a new full array with all the position of the array allocated but not instantiated, the funnction you pass is needed for istantiate a value when you need to eset an element in the array;
-extern void *get_element_reference(array *object,unsigned int position); //get a pointer with a copy od the data in a specific position of the array
-extern unsigned int set_value_in_position(array *object,void *value,int position); //instantiate the values in a specific position of the array
-extern unsigned int get_length(array *object);	//get the max number of items that you can put in the array
+// create a new full array with all the position of the array allocated but not instantiated, the funnction you pass is needed for istantiate a value when you need to set an element in the array;
+// dimensions_number_size 	: indicate the number of cells of the array
+// size_sigle_element		: indicate the sizeof of a single element of the array
+// allocation_function		: this function pointer indicate how to give the value to an alement in the array in a specific position 
+// 				(es. allocation_value_function(void *position, void *value) in this case i consider an array of integer
+// 				{
+//					*(int *)position=*(int *) value;
+// 				}
+array *new_array(unsigned int dimensions_number_size,unsigned int size_single_element,void (*allocation_value_function) (void *,void *));
+
+//return a poiter to a position of the array
+//if the operation is not possible because the position you indicate is out of the array
+//the function terminate with return NULL and print in output (stderr) an error message
+//otherwise the output of the function is the pointer to the data
+extern void *get_element_reference(array *object,unsigned int position); 
+
+//instantiate the values in a specific position of the array
+//if the operation is not possible because the position you indicate is out of the array
+//the function terminate with return 0 and print in output (stderr) an error message
+//otherwise the output of the function is 1
+extern unsigned int set_value_in_position(array *object,void *value,int position); 
+
+//return the the max number of items you can store in the array
+extern unsigned int get_length(array *object);	
+
+//return the current amount of items in the array
 extern unsigned int get_current_umount(array *object);
-extern void destroy(void *object); //destroy the array and put the pointer to NULL
+
+//destroy the array e set the pointer to NULL
+extern void destroy(void *object); 
 
 #endif
